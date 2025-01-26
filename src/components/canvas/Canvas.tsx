81df13b1c5e8dbc418054deb866db483031bd197
jcy97 <sook3427@naver.com>
2025-01-26 20:25:27 +0900

    [feat] Dragging

--- src/components/canvas/Canvas.tsx
+++ src/components/canvas/Canvas.tsx
@@ -15,7 +15,7 @@ import {
 } from "~/types";
 import { nanoid } from "nanoid";
 import { LiveObject } from "@liveblocks/client";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import React from "react";
 import ToolsBar from "../toolsbar/ToolsBar";
 
@@ -27,7 +27,7 @@ export default function Canvas() {
   const [canvasState, setState] = useState<CanvasState>({
     mode: CanvasMode.None,
   });
-  const [camrea, setCamera] = useState<Camera>({ x: 0, y: 0, zoom: 1 });
+  const [camera, setCamera] = useState<Camera>({ x: 0, y: 0, zoom: 1 });
   const insertLayer = useMutation(
     (
       { storage, setMyPresence },
@@ -75,19 +75,55 @@ export default function Canvas() {
     [],
   );
 
+  const onPointerDown = useMutation(
+    ({}, e: React.PointerEvent) => {
+      const point = pointerEventToCanvasPoint(e, camera);
+      if (canvasState.mode === CanvasMode.Dragging) {
+        setState({ mode: CanvasMode.Dragging, origin: point });
+      }
+    },
+    [camera, canvasState.mode, setState],
+  );
+  const onPointerMove = useMutation(
+    ({}, e: React.PointerEvent) => {
+      const point = pointerEventToCanvasPoint(e, camera);
+      if (
+        canvasState.mode === CanvasMode.Dragging &&
+        canvasState.origin !== null
+      ) {
+        const deltaX = e.movementX;
+        const deltaY = e.movementY;
+        setCamera((camera) => ({
+          x: camera.x + deltaX,
+          y: camera.y + deltaY,
+          zoom: camera.zoom,
+        }));
+      }
+    },
+    [canvasState, setState, insertLayer],
+  );
   const onPointerUp = useMutation(
     ({}, e: React.PointerEvent) => {
-      const point = pointerEventToCanvasPoint(e, camrea);
+      const point = pointerEventToCanvasPoint(e, camera);
       if (canvasState.mode === CanvasMode.None) {
         setState({ mode: CanvasMode.None });
       } else if (canvasState.mode === CanvasMode.Inserting) {
         console.log(canvasState.layerType);
         insertLayer(canvasState.layerType, point);
+      } else if (canvasState.mode === CanvasMode.Dragging) {
+        setState({ mode: CanvasMode.Dragging, origin: null });
       }
     },
     [canvasState, setState, insertLayer],
   );
 
+  const onWheel = useCallback((e: React.WheelEvent) => {
+    setCamera((camera) => ({
+      x: camera.x - e.deltaX,
+      y: camera.y - e.deltaY,
+      zoom: camera.zoom,
+    }));
+  }, []);
   return (
     <div className="flex h-screen w-full">
       <main className="fixed left-0 right-0 h-screen overflow-y-auto">
@@ -97,8 +133,18 @@ export default function Canvas() {
           }}
           className="h-full"
         >
-          <svg onPointerUp={onPointerUp} className="h-full w-full">
-            <g>
+          <svg
+            onWheel={onWheel}
+            onPointerUp={onPointerUp}
+            onPointerDown={onPointerDown}
+            onPointerMove={onPointerMove}
+            className="h-full w-full"
+          >
+            <g
+              style={{
+                transform: `translate(${camera.x}px, ${camera.y}px) scale(${camera.zoom})`,
+              }}
+            >
               {layerIds?.map((layerId) => (
                 <LayerComponent key={layerId} id={layerId} />
               ))}
@@ -109,6 +155,14 @@ export default function Canvas() {
       <ToolsBar
         canvasState={canvasState}
         setCanvasState={(newState) => setState(newState)}
+        zoomIn={() => {
+          setCamera((camera) => ({ ...camera, zoom: camera.zoom + 0.1 }));
+        }}
+        zoomOut={() => {
+          setCamera((camera) => ({ ...camera, zoom: camera.zoom - 0.1 }));
+        }}
+        canZoomIn={camera.zoom < 2}
+        canZoomOut={camera.zoom > 0.5}
       />
     </div>
   );
--- src/components/toolsbar/ToolsBar.tsx
+++ src/components/toolsbar/ToolsBar.tsx
@@ -1,13 +1,23 @@
 import { CanvasMode, CanvasState, LayerType } from "~/types";
 import SelectionButton from "./SelectionButton";
 import ShapesSelectionButton from "./ShapesSelectionButton";
+import ZoomInButton from "./ZoomInButton";
+import ZoomOutButton from "./ZoomOutButton";
 
 export default function ToolsBar({
   canvasState,
   setCanvasState,
+  zoomIn,
+  zoomOut,
+  canZoomIn,
+  canZoomOut,
 }: {
   canvasState: CanvasState;
   setCanvasState: (newState: CanvasState) => void;
+  zoomIn: () => void;
+  zoomOut: () => void;
+  canZoomIn: boolean;
+  canZoomOut: boolean;
 }) {
   return (
     <div className="fixed bottom-4 left-1/2 z-10 flex -translate-x-1/2 items-center justify-center rounded-lg bg-white p-1 shadow-[0_0_3px_rgba(0,0,0,0,18)]">
@@ -38,6 +48,11 @@ export default function ToolsBar({
             setCanvasState({ mode: CanvasMode.Inserting, layerType })
           }
         />
+        <div className="w-[1px] self-stretch bg-black/10" />
+        <div className="flex items-center justify-center">
+          <ZoomInButton onClick={zoomIn} disabled={!canZoomIn} />
+          <ZoomOutButton onClick={zoomOut} disabled={!canZoomOut} />
+        </div>
       </div>
     </div>
   );
